45acebbe0820afc7f4be518ad0eaf9183cc12496
XisUndefined <muhllis.ahmad@gmail.com>
2024-09-17 14:27:27 +0700

    feat: warmup-1 makes10

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import warmup1.*;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println(ParrotTrouble.parrotTrouble(true, 6)); // true
-    System.out.println(ParrotTrouble.parrotTrouble(true, 7)); // false
-    System.out.println(ParrotTrouble.parrotTrouble(false, 6)); // false
+    System.out.println(Makes10.makes10(9, 10)); // true
+    System.out.println(Makes10.makes10(9, 9)); // false
+    System.out.println(Makes10.makes10(1, 9)); // true
   }
 }
