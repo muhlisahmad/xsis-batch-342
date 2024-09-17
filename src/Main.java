bb671cd72afef2b8a450fb270d91fd5498ae343b
XisUndefined <muhllis.ahmad@gmail.com>
2024-09-17 14:05:30 +0700

    feat: warmup1 sumDouble

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import warmup1.SumDouble;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello Xsis");
+    System.out.println(SumDouble.sumDouble(1, 2)); // 3
+    System.out.println(SumDouble.sumDouble(3, 2)); // 5
+    System.out.println(SumDouble.sumDouble(2, 2)); // 8
   }
 }
---
+++ src/warmup1/SumDouble.java
@@ -0,0 +1,11 @@
+package warmup1;
+
+public class SumDouble {
+  public static int sumDouble(int a, int b) {
+    if (a == b) {
+      return 2 * (a + b);
+    } else {
+      return a + b;
+    }
+  }
+}
