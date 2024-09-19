e9d075a114b026c1b6b1177cc06e589f8a451bda
XisUndefined <muhllis.ahmad@gmail.com>
2024-09-19 13:48:07 +0700

    feat: adjustment

--- src/dailyQuest/Daily02.java
+++ src/dailyQuest/Daily02.java
@@ -22,48 +22,40 @@ public class Daily02 {
     }
   }
 
-  public static String dayNumber(String day) {
+  public static int dayNumber(String day) {
     String cleanStrDay = day.trim().toLowerCase();
     switch (cleanStrDay) {
       case "senin":
-        return "1";
+        return 1;
       case "selasa":
-        return "2";
+        return 2;
       case "rabu":
-        return "3";
+        return 3;
       case "kamis":
-        return "4";
+        return 4;
       case "jumat":
-        return "5";
+        return 5;
       case "sabtu":
-        return "6";
+        return 6;
       case "minggu":
-        return "7";
+        return 7;
       default:
-        return "Invalid day";
+        return -1;
     }
   }
 
   public static void weekComposition(String day) {
     String cleanStrDay = day.trim().toLowerCase();
-    if (!(cleanStrDay.equals("senin") ||
-          cleanStrDay.equals("selasa") ||
-          cleanStrDay.equals("rabu") ||
-          cleanStrDay.equals("kamis") ||
-          cleanStrDay.equals("jumat") ||
-          cleanStrDay.equals("sabtu") ||
-          cleanStrDay.equals("minggu")
-    )) {
+    int nthDay = dayNumber(cleanStrDay);
+    if (nthDay == -1) {
       System.out.println("Invalid day");
-    } else {
-      int nthDay = Integer.parseInt(dayNumber(cleanStrDay));
-  
-      System.out.println("kemarin lusa adalah hari " + dayName(nthDay <= 2 ? (7 - 2) + nthDay : nthDay - 2));
-      System.out.println("kemarin adalah hari " + dayName(nthDay == 1 ? 7 : nthDay - 1));
-      System.out.println("hari ini adalah hari " + cleanStrDay);
-      System.out.println("besok adalah hari " + dayName(nthDay == 7 ? 1 : nthDay + 1));
-      System.out.println("besok lusa adalah hari " + dayName(nthDay >= 6 ? 2 - (7 - nthDay) : nthDay + 2));
+      return;
     }
 
+    System.out.println("kemarin lusa adalah hari " + dayName(nthDay <= 2 ? (7 - 2) + nthDay : nthDay - 2));
+    System.out.println("kemarin adalah hari " + dayName(nthDay == 1 ? 7 : nthDay - 1));
+    System.out.println("hari ini adalah hari " + cleanStrDay);
+    System.out.println("besok adalah hari " + dayName(nthDay == 7 ? 1 : nthDay + 1));
+    System.out.println("besok lusa adalah hari " + dayName(nthDay >= 6 ? 2 - (7 - nthDay) : nthDay + 2));
   }
 }
