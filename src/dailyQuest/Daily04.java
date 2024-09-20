d63ed657ea4cf742b7912e8aeeb02238a63a068e
XisUndefined <muhllis.ahmad@gmail.com>
2024-09-20 09:29:37 +0700

    fix: sysout formatting

--- src/dailyQuest/Daily04.java
+++ src/dailyQuest/Daily04.java
@@ -12,16 +12,19 @@ public class Daily04 {
   public static void indexSquareArray(int n) {
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < n; j++) {
-        System.out.println("[" + i + ", " + j + "]");
+        System.out.print((i == 0 && j == 0) ? "[[" + i + ", " + j + "]; " : (j == 0) ? " [" + i + ", " + j + "]; " : (i == n - 1 && j == n - 1) ? "[" + i + ", " + j + "]]" : "[" + i + ", " + j + "]; ");
       }
+      System.out.println();
     }
   }
 
   public static void indexArray(int rows, int cols) {
     for (int i = 0; i < rows; i++) {
       for (int j = 0; j < cols; j++) {
-        System.out.println("[" + i + ", " + j + "]");
+        // System.out.print((j == 0) ? "[[" + i + ", " + j + "]" : (j == cols - 1) ? "[" + i + ", " + j + "]]" : "[" + i + ", " + j + "]");
+        System.out.print((i == 0 && j == 0) ? "[[" + i + ", " + j + "]; " : (j == 0) ? " [" + i + ", " + j + "]; " : (i == rows - 1 && j == cols - 1) ? "[" + i + ", " + j + "]]" : "[" + i + ", " + j + "]; ");
       }
+      System.out.println();
     }
   }
 
