97d086c6bc191400ecd85bbd174ed5fbbbdf070f
muhlisahmad <muhllis.ahmad@gmail.com>
2024-09-30 13:40:15 +0700

    fix: remove unnecessary import

--- src/oop/application/Daily09.java
+++ src/oop/application/Daily09.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
