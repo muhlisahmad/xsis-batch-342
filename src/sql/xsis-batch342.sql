2fc4d5c7c35393c72efada8dd474784c52871368
muhlisahmad <muhllis.ahmad@gmail.com>
2024-10-01 16:42:52 +0700

    feat: done insert data

--- src/sql/xsis-batch342.sql
+++ src/sql/xsis-batch342.sql
@@ -66,3 +66,55 @@ create table public.scores (
 		on update cascade,
 	score int not null
 );
+
+-- Insert lecturer_types seed data
+insert into public.lecturer_types (code, description)
+values ('T001', 'Tetap'), ('T002', 'Honorer'), ('T003', 'Expertise');
+
+-- Insert exams seed data
+insert into public.exams (code, name, status) values 
+('U001', 'Algoritma', true),
+('U002', 'Aljabar', true),
+('U003', 'Statistika', false),
+('U004', 'Etika Profesi', false),
+('U005', 'Bahasa Inggris', true);
+
+-- Insert religions seed data
+insert into public.religions (code, description) values
+('A001', 'Islam'),
+('A002', 'Kristen'),
+('A003', 'Katolik'),
+('A004', 'Hindu'),
+('A005', 'Budha');
+
+-- Insert majors seed data
+insert into public.majors (code, name, status) values
+('J001', 'Teknik Informatika', true),
+('J002', 'Management Informatika', true),
+('J003', 'Sistem Informasi', false),
+('J004', 'Sistem Komputer', true),
+('J005', 'Komputer Akuntasi', false);
+
+-- Insert lecturer seed data
+insert into public.lecturers (code, name, major_code, type_code) values
+('D001', 'Prof. Dr. Retno Wahyuningsih', 'J001', 'T002'),
+('D002', 'Prof. Roy M. Sutikno', 'J002', 'T001'),
+('D003', 'Prof. DrHendri A. Verburgh', 'J003', 'T002'),
+('D004', 'Prof. Risma Suparwata', 'J004', 'T002'),
+('D005', 'Prof. Amir Sjarifdudni Madjid, MM, MA', 'J005', 'T001');
+
+-- Insert students seed data
+insert into public.students (code, name, address, religion_code, major_code) values
+('M001', 'Budi Gunawan', 'Jl. Mawar No 3 RT 05 Cicalengka, Bandung', 'A001', 'J001'),
+('M002', 'Rinto Raharjo', 'Jl. Kebagusan No. 33 RT04 RW06 Bandung', 'A002', 'J002'),
+('M003', 'Asep Saepudin', 'Jl. Sumatera No. 12 RT02 RW01, Ciamis', 'A001', 'J003'),
+('M004', 'M. Hafif Isbullah', 'Jl. Jawa No 01 RT01 RW01, Jakarta Pusat', 'A001', 'J001'),
+('M005', 'Cahyono', 'Jl. Niagara No. 54 RT01 RW09, Surabaya', 'A003', 'J002');
+
+-- Insert scores seed data
+insert into public.scores (student_code, exam_code, score) values
+('M004', 'U001', 90),
+('M001', 'U002', 80),
+('M002', 'U003', 85),
+('M004', 'U002', 95),
+('M005', 'U005', 70);
