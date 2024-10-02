a66919d9352f15ea49314b531567dcb88f0aa26e
muhlisahmad <muhllis.ahmad@gmail.com>
2024-10-02 14:30:34 +0700

    feat: done inserting seed data

--- src/sql/xsis-batch342.sql
+++ src/sql/xsis-batch342.sql
@@ -244,7 +244,7 @@ create table company.biodata (
 	id bigserial primary key,
 	first_name varchar(20) not null,
 	last_name varchar(30) not null,
-	dob varchar(10) not null,
+	dob date not null,
 	pob varchar(50) not null,
 	address varchar(255) not null,
 	marital_status boolean not null
@@ -343,7 +343,7 @@ create table company.employee_leave (
 	id bigserial primary key,
 	employee_id bigint,
 	period varchar(4) not null,
-	regular_quote int not null,
+	regular_quota int not null,
 	foreign key (employee_id) references company.employee(id)
 		on delete cascade
 		on update cascade
@@ -358,3 +358,101 @@ create table company.contact_person (
 		on delete cascade
 		on update cascade
 );
+
+-- Data Insertion
+
+insert into company."position" (name) values
+('Direktur'),
+('General Manager'),
+('Manager'),
+('Staff');
+
+insert into company.biodata (first_name, last_name, dob, pob, address, marital_status) values
+('Raya', 'Indriyani', '1991-01-01', 'Bali', 'Jl. Raya Baru, Bali', false),
+('Rere', 'Wulandari', '1991-01-02', 'Bandung', 'Jl. Berkah Ramadhan, Bandung', false),
+('Bunga', 'Maria', '1991-03-03', 'Jakarta', 'Jl. Mawar Semerbak, Bogor', true),
+('Natasha', 'Wulan', '1990-04-10', 'Jakarta', 'Jl. Mawar Harum, Jakarta', false),
+('Zahra', 'Aurelia Putri', '1991-03-03', 'Jakarta', 'Jl. Mawar Semerbak, Bogor', true),
+('Katniss', 'Everdeen', '1989-01-12', 'Jakarta', 'Jl. Bunga Melati, Jakarta', true);
+
+insert into company.departement (name) values
+('Recruitment'),
+('Sales'),
+('Human Resource'),
+('General Affair');
+
+insert into company.travel_type (name, travel_fee) values
+('In Indonesia', 200000),
+('Out Indonesia', 350000);
+
+insert into company.leave (type, name) values
+('Regular', 'Cuti Tahunan'),
+('Khusus', 'Cuti Menikah'),
+('Khusus', 'Cuti Haji & Umroh'),
+('Khusus', 'Melahirkan');
+
+insert into company.employee (biodata_id, nip, status, salary) values
+(1, 'NX001', 'Permanen', 12000000),
+(2, 'NX002', 'Kontrak', 15000000),
+(4, 'NX003', 'Permanen', 13500000),
+(5, 'NX004', 'Permanen', 12000000),
+(6, 'NX005', 'Permanen', 17000000),
+(3, 'NX006', 'Kontrak', 15000000);
+
+insert into company.contact_person (biodata_id, type, contact) values
+(1, 'MAIL', 'raya.indriyani@gmail.com'),
+(1, 'PHONE', '085612345678'),
+(2, 'MAIL', 'rere.wulandari@gmail.com'),
+(2, 'PHONE', '081312345678'),
+(2, 'PHONE', '087812345678'),
+(3, 'MAIL', 'bunga.maria@gmail.com'),
+(4, 'MAIL', 'natasha.wulan@gmail.com'),
+(5, 'MAIL', 'zahra.putri@gmail.com'),
+(6, 'MAIL', 'katniss.everdeen@gmail.com');
+
+insert into company.employee_leave (employee_id, period, regular_quota) values
+(1, '2020', 16),
+(2, '2020', 12),
+(1, '2021', 16),
+(2, '2021', 12),
+(4, '2021', 12),
+(5, '2021', 12),
+(6, '2021', 12);
+
+insert into company.leave_request (employee_id, leave_id, start_date, end_date, reason) values
+(1, 1, '2020-10-10', '2020-10-12', 'Liburan'),
+(1, 1, '2020-11-12', '2020-11-15', 'Acara keluarga'),
+(2, 2, '2020-05-05', '2020-05-07', 'Menikah'),
+(2, 1, '2020-09-09', '2020-09-13', 'Touring'),
+(2, 1, '2020-12-24', '2020-12-25', 'Acara keluarga');
+
+insert into company.travel_request (employee_id, travel_type_id, start_date, end_date) values
+(1, 1, '2020-07-07', '2020-07-08'),
+(1, 1, '2020-08-07', '2020-08-08'),
+(2, 2, '2020-04-04', '2020-04-07');
+
+insert into company.travel_settlement (travel_request_id, item_name, item_cost) values
+(1, 'Tiket travel Do-Car berangkat', 165000),
+(1, 'Hotel', 750000),
+(1, 'Tiket travel Do-Car pulang', 165000),
+(2, 'Tiket pesawat berangkat', 750000),
+(2, 'Hotel', 650000),
+(2, 'Tiket pesawat pulang', 1250000),
+(3, 'Tiket pesawat berangkat', 4750000),
+(3, 'Hotel', 6000000),
+(2, 'Tiket pesawat pulang', 4250000);
+
+insert into company."family" (biodata_id, "name", status) values
+(3, 'Azka Fadlan Zikrullah', 'Suami'),
+(3, 'Primrose Everdeen', 'Anak'),
+(5, 'Jaka Samudera Buana', 'Suami'),
+(5, 'Friska Davira', 'Anak'),
+(5, 'Harum Indah Az Zahra', 'Anak'),
+(6, 'Adya Pratama Yuda', 'Suami');
+
+insert into company.employee_position (employee_id, position_id) values
+(5, 1),
+(4, 2),
+(3, 3),
+(2, 4),
+(1, 4);
